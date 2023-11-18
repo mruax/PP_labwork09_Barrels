b9ecfa83aecd349fb9d42f4f6d188218863c32fb
mruax <matvey.chernyakov@yandex.ru>
2023-11-18 15:48:59 +0500

    added basic logging

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ def get_barrels_amount():
 
     :return: int
     """
+    n = input("Введите количество бочонков в мешочке: ")
     try:
-        n = int(input("Введите количество бочонков в мешочке: "))
+        n = int(n)
         if n > 0:
             return n
         else:
+            logging.error(f"Incorrect input data. n = {n}. Error: not natural number")
             return False
     except Exception as error:
+        logging.error(f"Incorrect input data. n = {n}. Error: {error}")
         return False
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, filename="logs/barrels.log",
+                        filemode="w", format="%(asctime)s %(levelname)s %(message)s")
+
     print("Добро пожаловать в клуб любителей лото!")
 
     # Вводим число n пока не будет валидным
@@ -26,5 +32,5 @@ if __name__ == "__main__":
     while not n:
         print("Ошибка! Введенное число должно быть натуральным.")
         n = get_barrels_amount()
+    logging.info(f"Input data n = {n}.")
 
-    
