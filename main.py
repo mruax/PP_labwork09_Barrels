bc96ad9ac7b15b330b421f0a61e8101c1832d45c
mruax <matvey.chernyakov@yandex.ru>
2023-11-18 15:40:49 +0500

    added n input data with validation

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 from random import randint
 import logging
 
+
+def get_barrels_amount():
+    """
+    Функция для получения количества бочонков в мешке. Проверяет данные на корректность.
+
+    :return: int
+    """
+    try:
+        n = int(input("Введите количество бочонков в мешочке: "))
+        if n > 0:
+            return n
+        else:
+            return False
+    except Exception as error:
+        return False
+
+
+if __name__ == "__main__":
+    print("Добро пожаловать в клуб любителей лото!")
+
+    # Вводим число n пока не будет валидным
+    n = get_barrels_amount()
+    while not n:
+        print("Ошибка! Введенное число должно быть натуральным.")
+        n = get_barrels_amount()
+
+    
